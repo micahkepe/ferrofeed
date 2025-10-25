f878f759b89f4e5b9968465c26ad93136a1a1ffb
micahkepe <micahkepe@gmail.com>
2025-10-25 15:08:03 -0500

    test(cli): add `assert_cmd` dev-dependency for testing CLI

--- tests/cli_test.rs
+++ tests/cli_test.rs
@@ -1,13 +1,34 @@
 //! Integration tests for CLI commands.
 
+use assert_cmd::Command;
+
 use ferrofeed::{commands, db::Db};
 
+/// Create a test database. ":memory:" is used for in-memory database that is volatile and
+/// will be deleted when the process exits.
+///   See: <https://www.sqlite.org/inmemorydb.html>
 fn create_test_db() -> Db {
     let db = Db::open(":memory:").expect("failed to create test db");
     db.init_feed_table().expect("failed to init feed table");
     db.init_feed_item_table()
         .expect("failed to init feed_item table");
     db
+}
+
+/// Helper function to run the `query` binary with the given arguments and return a
+/// [`assert_cmd::assert::Assert`].
+fn run_ferrofeed_command(args: &[&str]) -> assert_cmd::assert::Assert {
+    let mut cmd = Command::cargo_bin("ferrofeed").expect("Failed to find binary");
+    cmd.args(args);
+    cmd.assert()
+}
+
+#[test]
+fn test_run_help() {
+    let help_args_options = vec!["-h", "--help"];
+    for arg in help_args_options {
+        run_ferrofeed_command(&[arg]).success();
+    }
 }
 
 #[test]
